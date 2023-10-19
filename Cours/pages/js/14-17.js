90402b696b247cb8ef704b05ae2e001f59ee5846
Johnny Piette <johnny.piette@gmail.com>
2023-10-19 02:01:42 +0200

    Updates

--- Cours/pages/js/14-17.js
+++ Cours/pages/js/14-17.js
@@ -1,34 +1,40 @@
-function btnUpdateClick() {
+document.addEventListener("DOMContentLoaded", function () {
+  function btnUpdateClick() {
     let btnUpdate = document.getElementById("btnUpdate");
     let btnSubmit = document.getElementById("btnSubmit");
     let form = document.getElementById("form");
     if (btnUpdate.getAttribute("mode") == "view") {
-        setReadOnly(false);
-        btnUpdate.setAttribute("mode", "edit");
-        btnUpdate.setAttribute("aria-valuetext", "Sortir du mode édition");
-        btnUpdate.value = "Sortir du mode édition";
-        if (form.getAttribute("updated") == "true") {
-            btnSubmit.removeAttribute("hidden");
-        }
-        addInputListeners();
+      setReadOnly(false);
+      btnUpdate.setAttribute("mode", "edit");
+      btnUpdate.setAttribute("aria-valuetext", "Sortir du mode édition");
+      btnUpdate.value = "Sortir du mode édition";
+      if (form.getAttribute("updated") == "true") {
+        btnSubmit.removeAttribute("hidden");
+      }
+      addInputListeners();
     } else {
-        setReadOnly(true);
-        btnUpdate.setAttribute("mode", "view");
-        btnUpdate.setAttribute("aria-valuetext", "Passer en mode édition");
-        btnUpdate.value = "Passer en mode édition";
-        btnSubmit.setAttribute("hidden", "");
+      setReadOnly(true);
+      btnUpdate.setAttribute("mode", "view");
+      btnUpdate.setAttribute("aria-valuetext", "Passer en mode édition");
+      btnUpdate.value = "Passer en mode édition";
+      btnSubmit.setAttribute("hidden", "");
     }
-}
+  }
 
-function setReadOnly(state) {
-    document.querySelectorAll("input").forEach(input => input.readOnly = state);
-}
+  function setReadOnly(state) {
+    document
+      .querySelectorAll("input")
+      .forEach((input) => (input.readOnly = state));
+  }
 
-function addInputListeners() {
-    document.querySelectorAll("input").forEach(input => {
-        input.addEventListener("input", function () {
-            document.getElementById("btnSubmit").removeAttribute("hidden");
-            document.getElementById("form").setAttribute("updated", "true");
-        });
+  function addInputListeners() {
+    document.querySelectorAll("input").forEach((input) => {
+      input.addEventListener("input", function () {
+        document.getElementById("btnSubmit").removeAttribute("hidden");
+        document.getElementById("form").setAttribute("updated", "true");
+      });
     });
-}
+  }
+
+  btnUpdate.addEventListener("click", btnUpdateClick);
+});
